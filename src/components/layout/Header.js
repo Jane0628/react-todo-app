98b51a67914d71557e14ac229074d4e7fdd9ddca
Jane <ribbon0628@naver.com>
2023-06-27 12:16:24 +0900

    23.06.27 Bug fix

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,26 +1,16 @@
-import { AppBar, Toolbar, Grid, Typography, Button } from "@mui/material";
-import React, { useEffect, useState } from 'react'
+import { AppBar, Toolbar, Grid, Typography } from "@mui/material";
+import React, { useContext } from 'react'
 import './header.css';
-import { Link, useNavigate } from "react-router-dom";
-import { isLogin, getLoginUserInfo } from "../../util/login-utils"
+import { Link } from "react-router-dom";
+import AuthContext from "../../util/AuthContext";
 
 const Header = () => {
 
-  const redirection = useNavigate();
+  // AuthContext에서 로그인 상태와 onLogout 함수를 가져옵니다.
+  const { isLoggedIn, onLogout, userName } = useContext(AuthContext);
 
-  const [userInfo, setUserInfo] = useState({});
-
-  const { token, userName, role } = userInfo;
-
-  // 로그아웃 핸들러
-  const logoutHandler = e => {
-    localStorage.clear();
-    redirection('/login');
-  }
-
-  useEffect(() => {
-    setUserInfo(getLoginUserInfo());
-  }, []);
+  // AuthContext의 onLogout 함수를 호출하여 로그인 상태를 업데이트합니다.
+  onLogout();
 
   return (
     <AppBar position="fixed" style={{
@@ -37,15 +27,15 @@ const Header = () => {
               }
             }>
               <Typography variant="h4">
-                {isLogin() ? userName + '님' : '오늘'}의 할 일
+                {isLoggedIn ? userName + '님' : '오늘'}의 할 일
               </Typography>
             </div>
           </Grid>
 
           <Grid item>
             <div className="btn-group">
-              {isLogin() ?
-                (<button className="logout-btn" onClick={logoutHandler}>로그아웃</button>)
+              {isLoggedIn ?
+                (<button className="logout-btn" onClick={onLogout()}>로그아웃</button>)
                 :
                 (<>
                   <Link to={'/login'}>로그인</Link>
--- src/components/todo/TodoTemplate.js
+++ src/components/todo/TodoTemplate.js
@@ -89,7 +89,7 @@ const TodoTemplate = () => {
         // console.log(json);
 
         // fetch를 통해 받아온 데이터를 상태 변수에 할당.
-        if (!!json) setTodos(json.todos);
+        if (json) setTodos(json.todos);
 
         // 로딩 완료 처리
         setLoading(false);
